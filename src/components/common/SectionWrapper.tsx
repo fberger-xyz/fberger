f3bf19566dccca256307ecbf73a218077aa47cb7
fberger <frcs.berger@gmail.com>
2025-01-07 11:02:47 +0100

    feat: setup

--- src/components/common/SectionWrapper.tsx
+++ src/components/common/SectionWrapper.tsx
@@ -4,13 +4,12 @@ import { ReactNode } from 'react'
 export const BulletPoint = () => <span className="w-min pr-2 text-inactive">&#x2022;</span>
 export const TextWithBulletPoint = (props: { children?: ReactNode }) => {
     return (
-        <div className="flex items-start gap-1">
+        <div className="flex items-baseline gap-0.5">
             <BulletPoint />
-            <p>{props.children}</p>
+            <p className="flex flex-wrap items-baseline gap-1.5">{props.children}</p>
         </div>
     )
 }
-export const bulletPointClassNames = 'flex gap-1.5 items-center flex-wrap'
 export function SectionWrapper(props: { title: string; children: ReactNode; ulClassname?: string }) {
     return (
         <div className="flex w-full flex-col gap-1">
