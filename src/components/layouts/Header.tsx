c822de1efc0d46182d3eb8b90cb0518dc787ec05
fberger <frcs.berger@gmail.com>
2025-01-08 16:05:48 +0100

    feat: setup

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -12,7 +12,7 @@ export default function Header(props: { className?: string }) {
                 <div className="bg-background/10 absolute inset-0 z-40 rounded-xl" />
                 <LinkWrapper href="/" className="z-50 flex items-end gap-2">
                     <Image src={APP_METADATA.PROFILE_PICTURE} width={44} height={44} alt="https://x.com/fberger_xyz/photo" className="rounded-xl" />
-                    <p className="mb-1 hidden text-2xl text-primary md:flex">{APP_METADATA.SITE_AUTHOR}</p>
+                    <p className="mb-1 hidden text-2xl text-primary md:flex">{APP_METADATA.SITE_NAME}</p>
                 </LinkWrapper>
                 <div className="z-50 flex gap-0.5 sm:gap-1">
                     {APP_PAGES.filter((page) => page.inHeader).map((page) => (
