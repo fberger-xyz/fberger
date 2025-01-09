e4ba8cc72aeee7b4c82dc8850a8ba76056f0a1a6
fberger <frcs.berger@gmail.com>
2025-01-09 17:23:33 +0100

    feat: setup

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -16,7 +16,7 @@ export default function Header(props: { className?: string }) {
                 </LinkWrapper>
                 <LinkWrapper href="/" className="hidden items-center gap-3 md:z-50 md:flex">
                     <Image src={APP_METADATA.PROFILE_PICTURE} width={50} height={50} alt="https://x.com/fberger_xyz/photo" className="rounded-2xl" />
-                    <p className="mb-1 hidden text-2xl text-primary md:flex">{APP_METADATA.SITE_NAME}</p>
+                    <p className="mb-1 hidden text-2xl text-primary md:flex lg:font-bold">{APP_METADATA.SITE_NAME}</p>
                 </LinkWrapper>
                 <div className="z-50 flex gap-0.5 sm:gap-1">
                     {APP_PAGES.filter((page) => page.inHeader).map((page) => (
