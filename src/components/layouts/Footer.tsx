748b2ee9852ad79e055ab4039920457f4bd8e665
fberger <frcs.berger@gmail.com>
2025-02-13 15:50:21 +0100

    feat: setup

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,16 +1,16 @@
 'use client'
 
 import { cn } from '@/utils'
-import LinkWrapper from '../common/LinkWrapper'
 import { APP_METADATA } from '@/config/app.config'
 import IconWrapper from '../common/IconWrapper'
 import { IconIds } from '@/enums'
 import Image from 'next/image'
+import LinkWithIcon from '../common/LinkWithIcon'
 
 export default function Footer(props: { className?: string }) {
     return (
         <div className={cn('w-full flex justify-center text-sm border-t-2 border-light-hover bg-background/30', props.className)}>
-            <div className="m-8 flex w-full max-w-[600px] flex-col justify-center gap-5 p-6 sm:max-w-[700px]">
+            <div className="m-8 flex w-full max-w-[600px] flex-col justify-center gap-3 p-6 sm:max-w-[700px]">
                 <div className="flex items-center gap-3">
                     <Image src={APP_METADATA.PROFILE_PICTURE} width={56} height={56} alt="https://x.com/fberger_xyz/photo" className="rounded-full" />
                     <div className="flex h-min flex-col justify-center">
@@ -18,7 +18,7 @@ export default function Footer(props: { className?: string }) {
                         <p className="text-inactive">Passionate 10x fullstack developer</p>
                     </div>
                 </div>
-                <div className="flex gap-4">
+                {/* <div className="flex gap-4">
                     {[
                         { href: `https://t.me/${APP_METADATA.SOCIALS.TELEGRAM}`, icon: IconIds.TELEGRAM_LOGO },
                         { href: `https://x.com/${APP_METADATA.SOCIALS.X}`, icon: IconIds.X },
@@ -28,6 +28,29 @@ export default function Footer(props: { className?: string }) {
                         <LinkWrapper key={link.href} target="_blank" href={link.href} className="cursor-alias text-inactive hover:text-primary">
                             <IconWrapper icon={link.icon} className="size-6" />
                         </LinkWrapper>
+                    ))}
+                </div> */}
+                <div className="flex flex-wrap items-center gap-2">
+                    {[
+                        { href: `https://t.me/${APP_METADATA.SOCIALS.TELEGRAM}`, icon: IconIds.TELEGRAM_LOGO, id: APP_METADATA.SOCIALS.TELEGRAM },
+                        { href: `https://x.com/${APP_METADATA.SOCIALS.X}`, icon: IconIds.X, id: '' },
+                        {
+                            href: `https://www.linkedin.com/in/${APP_METADATA.SOCIALS.LINKEDIN}`,
+                            icon: IconIds.LINKEDIN,
+                            id: '',
+                        },
+                        { href: `https://github.com/${APP_METADATA.SOCIALS.GITHUB}`, icon: IconIds.GITHUB, id: '' },
+                    ].map((link) => (
+                        <LinkWithIcon
+                            key={link.href}
+                            href={link.href}
+                            className={
+                                link.icon === IconIds.TELEGRAM_LOGO ? 'border-2 border-telegram bg-telegram/10 py-1 text-telegram' : 'border-2 py-1'
+                            }
+                        >
+                            {link.id && <p className="pr-1 text-base font-bold">{link.id}</p>}
+                            <IconWrapper icon={link.icon} className="size-6" />
+                        </LinkWithIcon>
                     ))}
                 </div>
             </div>
